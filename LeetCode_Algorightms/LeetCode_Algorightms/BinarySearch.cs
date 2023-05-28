b5cbb9cb660ed4efd3ea86db9ede400a238bf2fd
Rafiqul Iislam Robin <kratosrobin467@gmail.com>
2023-05-28 14:16:20 +0600

    binarySearch

---
+++ LeetCode_Algorightms/LeetCode_Algorightms/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Algorightms
+{
+    public static class BinarySearch
+    {
+        public static int Search(int[] nums, int target)
+        {
+
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+                if (nums[middle] == target)
+                {
+                    return middle;
+                }
+                else if (nums[middle] < target)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode_Algorightms/LeetCode_Algorightms/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        var target = Convert.ToInt32(Console.ReadLine());
+
+
+    }
+
+
+}
